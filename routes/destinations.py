23d58bf673b6cb3de4876766c6b559b8eb3b913d
aylafmny2-cmd <aylafmny2@gmail.com>
2026-03-14 07:33:38 +0300

    added destinations API

--- routes/destinations.py
+++ routes/destinations.py
@@ -15,3 +15,10 @@ def search_destinations(city: str = "", category: str = ""):
     if category: query["category"] = {"$regex": category, "$options": "i"}
     results = list(destinations_collection.find(query, {"_id": 0}))
     return {"count": len(results), "data": results}
+
+@router.get("/{name}")
+def get_destination(name: str):
+    place = destinations_collection.find_one({"name": name}, {"_id": 0})
+    if not place:
+        return {"error": "المكان غير موجود"}
+    return place
