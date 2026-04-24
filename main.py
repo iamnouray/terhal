3270bf4225b2958e23dbdf8ba8450034a8407582
shadoni <shadoni@shadonis-MacBook-Air.local>
2026-04-25 02:04:35 +0300

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import destinations, users, recommendations, likes, reviews
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(destinations.router)
 app.include_router(users.router)
 app.include_router(recommendations.router)
 app.include_router(likes.router)
 app.include_router(reviews.router)
+
